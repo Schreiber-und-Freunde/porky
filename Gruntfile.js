4ba53c634a86a19a6cb8860c313eeb1e9f42359a
Philipp Schreiber <Schreiber & Freunde GmbH>
2014-09-15 15:32:02 +0200

    adds grunt

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'app/porky/tests/**/*.jsx', 'app/porky/bin/**/*.jsx']
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+
+};
